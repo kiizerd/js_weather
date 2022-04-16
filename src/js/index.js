281d19b08b75216dc5e491b2819345a778e1b280
kiizerd <ajsewell13@gmail.com>
2022-04-15 21:56:34 -0500

    Tied everything together, initialized, and updated at interval.

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,74 @@
-import '.././styles/main.scss';
+import '../styles/main.scss';
+import { Now } from 'temporal-polyfill';
+import forwardGeocodeQuery from './geocode';
+import { getWeatherData } from './weather';
+import getElements from './elements';
+import {
+  getDescriptionFromWeatherData,
+  getTempsFromWeatherData,
+  getWindFromWeatherData,
+  getDetailsFromWeatherData,
+  getDateTimeString,
+} from './getters';
 
-const openWeatherUrl      = 'http://api.openweathermap.org/';
-const openWeatherEndpoint = 'data/2.5/weather?q=London';
-// &APPID=1111111111'
+import { fillPageWithWeatherData } from './fillers';
 
-function component() {
-  const element = document.createElement('div');
+// Add:
+// Precipitation
 
-  element.textContent = 'Hello Webpack!';
-  element.classList.add('hello');
+const tickClockElement = () => {
+  const elements = getElements();
+  elements.clock.dateTime = Now.instant().toString();
+  elements.clock.textContent = getDateTimeString();
+};
 
-  return element;
-}
+const updateWeatherData = (lat, lon, name) => {
+  getWeatherData(lat, lon).then((data) => {
+    const { current, daily } = data;
+    const iconCode = current.weather[0].icon;
+    const weather = getDescriptionFromWeatherData(current.weather[0]);
+    const temps = getTempsFromWeatherData({ current, daily: daily[0] });
+    const wind = getWindFromWeatherData(current);
+    const details = getDetailsFromWeatherData(current);
+    const weatherData = {
+      name, weather, temps, wind, details, iconCode, daily,
+    };
+
+    fillPageWithWeatherData(weatherData);
+  });
+};
+
+const updateWeatherDataWithQuery = async (query) => {
+  const [lat, lon, name] = await forwardGeocodeQuery(query).then((geoData) => {
+    const queryData = geoData.data[0];
+    return [queryData.latitude, queryData.longitude, queryData.name];
+  });
+
+  updateWeatherData(lat, lon, name);
+};
+
+// Initialize App
+
+// Call once for initial load.
+tickClockElement();
+let lastQuery = 'London';
+updateWeatherDataWithQuery(lastQuery);
+
+document.getElementById('submit').onclick = async () => {
+  const search = document.getElementById('search');
+  const query = search.value;
+  lastQuery = query;
+  search.value = '';
+
+  updateWeatherDataWithQuery(query);
+};
+
+// Update weather every minute.
+setInterval(async () => {
+  updateWeatherDataWithQuery(lastQuery);
+}, 60000);
+
+// Update clock every second.
+setInterval(() => {
+  tickClockElement();
+}, 1000);
